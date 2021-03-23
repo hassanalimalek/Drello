6c07d4ee23916708909e137c50b028cd624ad328
Hassan Ali <hassanalimalek9@gmail.com>
2021-03-23 22:59:51 +0500

    Initial Loader Added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Board from './components/board';
 import SideBar from './components/sideBar';
 import IntroModal from './components/introModal'
 
+import { css } from "@emotion/core";
+import HashLoader from "react-spinners/HashLoader";
+
 
 
 import { useState } from 'react'
@@ -12,6 +15,14 @@ import { useState } from 'react'
 function App() {
 
   let [sideBarState,setSideBarState] = useState("hide");
+  
+  let [loading, setLoading] = useState(true);
+  let color = ("#111");
+  const override = css`
+  display: block;
+  color:black;
+  margin: 0 auto;
+`;
 
 
   let showSideBar = ()=>{
@@ -20,20 +31,30 @@ function App() {
   let hideSideBar = ()=>{
     setSideBarState("hide");
   }
+  setTimeout(()=>{
+    setLoading(false);
+  },3000)
 
  
   
-
-
-  return (
-    <div >
-      <IntroModal/>
-      <Header/>
-      <Subheader showSideBar = {showSideBar} />
-      <Board/>
-      <SideBar sideBarState={sideBarState} hideSideBar={hideSideBar} />
-    </div>
-  );
+  if(loading){
+    return (
+      <div className="loaderContainer">
+         <HashLoader color={color} loading={loading} css={override} size={80} />
+      </div>
+    )
+  }
+  else{
+    return (
+      <div >
+        <IntroModal/>
+        <Header/>
+        <Subheader showSideBar = {showSideBar} />
+        <Board/>
+        <SideBar sideBarState={sideBarState} hideSideBar={hideSideBar} />
+      </div>
+    );
+  }
 }
 
 export default App;
